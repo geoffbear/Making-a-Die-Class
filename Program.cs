a00b9306fa0392caf5e66a73f47161e950b1aebb
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-03-19 13:52:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Making_a_Die_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1;
+            Die die2;
+            die1 = new Die();
+            die2 = new Die();
+            die1.RollDie();
+            die1.DrawRoll();
+
+            die2.RollDie();
+            die2.DrawRoll();
+            Console.ReadLine();
+        }
+    }
+}
